6a599f22c1efb3561a4a73495a5fcad2f90be779
Kent Hansen <kenthansen98@gmail.com>
2023-01-14 17:08:34 -0600

    add favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,11 @@ export default function Head() {
     <>
       <title>Metro Explorer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="An app to explore metro systems across the globe" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="An app to explore metro systems across the globe"
+      />
+      <link rel="icon" href="/metro.ico" />
     </>
-  )
+  );
 }
